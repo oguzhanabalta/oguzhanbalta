aee787a5b655f6537cc658b46ad02208257988ce
oguzhan <oguzhanbalta99@gmail.com>
2022-04-30 22:14:01 +0300

    fix

--- components/Experience.js
+++ components/Experience.js
@@ -91,7 +91,7 @@ export default function Experience() {
             </Grid>
             <Grid container item xs={12} lg={6} direction="column" spacing={1} alignItems={align}>
                 {
-                    Object.getOwnPropertyNames(experience).map((title, id) =>
+                    experience.map((title, id) =>
                         <Grid item key={id} className={classes.expObj}>
                             <Typography variant="h4" align={"center"} gutterBottom component="p">
                                 {title}
